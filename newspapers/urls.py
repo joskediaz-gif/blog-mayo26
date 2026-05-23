fe22b1396e1d9f0aff76f18ffdb3c4c21bbe645f
jorge <joskediaz@gmail.com>
2026-05-23 17:26:37 -0400

    post detail is done

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BloglistView
+from .views import BloglistView, BlogDetailView
 urlpatterns = [
     path('', BloglistView.as_view(), name='blog-list'), 
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
 ]
