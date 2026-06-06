ef5d323bc3fc4e1c22a876c7e41fd942e5b1a7d9
jorge <joskediaz@gmail.com>
2026-06-06 17:17:24 -0400

    signup is done

--- base_project/urls.py
+++ base_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newspapers.urls')),  
-    path('accounts/', include('django.contrib.auth.urls')),     
+    path('accounts/', include('django.contrib.auth.urls')),   
+    path('accounts/', include('accounts.urls')),  
 ]
