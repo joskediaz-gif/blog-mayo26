fbce3999554408962947ca0bd9ffda9ae2a5ec9a
jorge <joskediaz@gmail.com>
2026-06-06 16:25:34 -0400

    login is done

--- base_project/urls.py
+++ base_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('newspapers.urls')),       
+    path('', include('newspapers.urls')),  
+    path('accounts/', include('django.contrib.auth.urls')),     
 ]
